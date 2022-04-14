c7829a95e804f722bf69d9716b264cfa86e170bb
juliana.goia <juliana.goia@allycode.com.br>
2022-04-13 23:07:04 -0300

    inciando crud de clientes e edição do readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# project-crudphp
-Construir um crud basico
+# CRUD PHP
+
+O projeto consiste em uma aplicação de cadastro de pedidos de compra, a partir de uma modelagem inicial, com as seguintes funcionalidades:
+
+- CRUD de clientes.
+- CRUD de produtos.
+- CRUD de pedidos de compra, com status (Em Aberto, Pago ou Cancelado).
+
+Cada CRUD:
+- deve ser filtrável e ordenável por qualquer campo, e possuir paginação de 20 itens.
+- deve possuir formulários para criação e atualização de seus itens.
+- deve permitir a deleção de qualquer item de sua lista.
+- Barra de navegação entre os CRUDs.
+- Links para os outros CRUDs nas listagens (Ex: link para o detalhe do cliente da compra na lista de pedidos de compra)
+
--- _clients/client-read.php
+++ _clients/client-read.php
@@ -13,17 +13,20 @@ require_once('../database/connection.php');
         </button>
     </div>
 
-    <table class="table table-hover">
+    <table class="table table-hover" id="clientTable">
         <thead>
             <tr>
-                <th scope="col">Nome</th>
-                <th scope="col">CPF</th>
-                <th scope="col">E-mail</th>
-                <th scope="col">Ações</th>
+                <th onclick="sortTable(0)">Nome</th>
+                <th>CPF</th>
+                <th onclick="sortTable(1)">E-mail</th>
+                <th>Ações</th>
             </tr>
         </thead>
         <tbody>
             <?php
+                // $q = select c.nome_cliente from clients c inner join orders o on c.nome_cliente = o.id_client;
+                // $busca = $banco->query($q);
+
                 $busca = $banco->query("select * from clients order by nome_cliente");
                 if(!$busca) {
                     // mensagem de erro ao tentar buscar os registros na tabela
@@ -65,24 +68,6 @@ require_once('../database/connection.php');
         </tbody>
     </table>
 
-    <nav aria-label="Page navigation example">
-        <ul class="pagination pagination-sm justify-content-center mt-4">
-            <li class="page-item">
-              <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-              </a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                  <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-</nav>
-
     <!-- Modal Cadastrar Novo Cliente -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg">
@@ -92,74 +77,46 @@ require_once('../database/connection.php');
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form class="row">
+                    <form class="row" action="client-read.php" method="POST" id="createNewClient">
                         <div class="mb-3">
                             <label for="nome" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="nome" placeholder="Nome">
+                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" require>
                         </div>
                         <div class="mb-3 col-6">
                             <label for="cpf" class="form-label">CPF</label>
-                            <input type="cpf" class="form-control" id="cpf" placeholder="CPF">
+                            <input type="cpf" class="form-control" id="cpf" name="cpf" placeholder="CPF">
                         </div>
                         <div class="mb-3 col-6">
                             <label for="email" class="form-label">E-mail</label>
-                            <input type="email" class="form-control" id="email" placeholder="E-mail">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="E-mail">
                         </div>
                     
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                    <button type="submit" class="btn btn-primary">Cadastrar</button>
+                    <button type="submit" class="btn btn-primary" id="registerNewClient">Cadastrar</button>
                 </div>
                 </form>
             </div>
         </div>
     </div>
-    <!-- Modal Editar Cliente -->
-    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Editar Cliente</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form class="row">
-                        <div class="mb-3">
-                            <label for="nome" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="nome" placeholder="Nome">
-                        </div>
-                        <div class="mb-3 col-6">
-                            <label for="cpf" class="form-label">CPF</label>
-                            <input type="cpf" class="form-control" id="cpf" placeholder="CPF">
-                        </div>
-                        <div class="mb-3 col-6">
-                            <label for="email" class="form-label">E-mail</label>
-                            <input type="email" class="form-control" id="email" placeholder="E-mail">
-                        </div>
-                    
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                    <button type="submit" class="btn btn-primary">Cadastrar</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
+
 </div>
 
 <script>
- function deleteClient() {
-     return 
-     console.log('clicou') 
-     Swal.fire({
-        title: 'Error!',
-        text: 'Do you want to continue',
-        icon: 'error',
-        confirmButtonText: 'Cool'
-    })
- }
+     $('#registerNewClient').on('click', function() {
+         if($('#nome').val == "") {
+             alert('Preencha o nome');
+             return false;
+         } else if ($('#cpf').val == "") {
+            alert('Preencha o CPF');
+             return false;
+         } if($('#email').val == "") {
+            alert('Preencha o email');
+             return false;
+         }
+         $('#createNewClient').submit();
+     })
+
 </script>
 
 <style>
